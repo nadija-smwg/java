25d5592fd868faf356705d8d8a3047bd0a374b05
Nadijasmwg <nadijasmwg@gmail.com>
2025-11-20 19:38:13 +0530

    11/20

---
+++ _11_break.java
@@ -0,0 +1,17 @@
+
+
+public class _11_break {
+
+   public static void main(String args[]) {
+      int x = 10;
+
+      while( x < 20 ) {
+         if(x == 15){
+            break;		 
+         }	     
+         System.out.print("value of x : " + x );
+         x++;
+         System.out.print("\n");
+      }
+   }
+}
